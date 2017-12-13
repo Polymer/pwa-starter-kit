bfd7e884406d163b41db4a7c891bd4e0b18cc0e4
Monica Dinculescu <noms@google.com>
2017-12-13 14:18:08 -0800

    remove commented out code

--- src/store/store.js
+++ src/store/store.js
@@ -5,8 +5,6 @@ import combineReducers from '../../../node_modules/@0xcda7a/redux-es6/es/combine
 import thunk from '../../../node_modules/redux-thunk/es/index.js';
 
 import app from './reducers/app.js';
-// import counter from './reducers/counter.js';
-// import shop from './reducers/shop.js';
 
 const compose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || origCompose;
 
@@ -20,7 +18,7 @@ store.addReducers({
   app
 });
 
-// This is used to lazy-load reducers
+// This is used to lazy-load reducers.
 function lazyReducerEnhancer(nextCreator) {
   return (origReducer, preloadedState) => {
     let lazyReducers = {};
