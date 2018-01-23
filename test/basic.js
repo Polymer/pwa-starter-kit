306acc45540c67a3ed50ddf97987e7c374ed9de4
Monica Dinculescu <noms@google.com>
2018-01-23 10:58:51 -0800

    router tests in a different way

--- test/basic.js
+++ test/basic.js
@@ -45,6 +45,22 @@ it('the app looks right with the eyeballs', function() {
   })();
 });
 
+it('the page selector switches pages', function(done) {
+  (async () => {
+    const browser = await puppeteer.launch({headless:false});
+    const page = await browser.newPage();
+    await page.goto(`${appUrl}`);
+    await page.waitForSelector('my-app', {visible: true});
+
+    await testNavigation(page, 'view2', 'View Two');
+    await testNavigation(page, 'view3', 'View Three');
+    await testNavigation(page, 'view1', 'View One');
+
+    done();
+    await browser.close();
+  })();
+});
+
 async function testNavigation(page, href, linkText) {
   const selector = `a[name="${href}"]`;
   const shadowSelector = `a[name="${href}"]`;
@@ -60,16 +76,57 @@ async function testNavigation(page, href, linkText) {
   expect(newUrl).equal(`${appUrl}/${href}`);
 }
 
-it('the page selector switches pages', function(done) {
+it('the page selector switches pages in a different way', function(done) {
   (async () => {
     const browser = await puppeteer.launch({headless:false});
     const page = await browser.newPage();
     await page.goto(`${appUrl}`);
     await page.waitForSelector('my-app', {visible: true});
 
-    await testNavigation(page, 'view2', 'View Two');
-    await testNavigation(page, 'view3', 'View Three');
-    await testNavigation(page, 'view1', 'View One');
+    // Setup
+    await page.evaluate(() => {
+      window.deepQuerySelector = function(query) {
+        const parts = query.split('::shadow');
+        let el = document;
+        for (let i = 0; i < parts.length; i++) {
+          el = el.querySelector(parts[i]);
+          if (i % 2 === 0) {
+            el = el.shadowRoot;
+          }
+        }
+        return el === document ? null : el;
+      }
+      console.log(window.deepQuerySelector);
+    });
+
+    // Navigate to View2.
+    // Problem: you can't pass these queries as an argument to a function, since
+    // those would be in the node context, not in the browser context we're running them.
+    let linkHandle = await page.evaluateHandle(() => window.deepQuerySelector('my-app::shadow a[name="view2"]'));
+    let linkText = await page.evaluate((el) => el.textContent, linkHandle);
+    expect(linkText).equal('View Two');
+
+    await linkHandle.click();
+    let newUrl = await page.evaluate('window.location.href')
+    expect(newUrl).equal(`${appUrl}/view2`);
+
+    // Navigate to View3.
+    linkHandle = await page.evaluateHandle(() => window.deepQuerySelector('my-app::shadow a[name="view3"]'));
+    linkText = await page.evaluate((el) => el.textContent, linkHandle);
+    expect(linkText).equal('View Three');
+
+    await linkHandle.click();
+    newUrl = await page.evaluate('window.location.href')
+    expect(newUrl).equal(`${appUrl}/view3`);
+
+    // Navigate to View1.
+    linkHandle = await page.evaluateHandle(() => window.deepQuerySelector('my-app::shadow a[name="view1"]'));
+    linkText = await page.evaluate((el) => el.textContent, linkHandle);
+    expect(linkText).equal('View One');
+
+    await linkHandle.click();
+    newUrl = await page.evaluate('window.location.href')
+    expect(newUrl).equal(`${appUrl}/view1`);
 
     done();
     await browser.close();
