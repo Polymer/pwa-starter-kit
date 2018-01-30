c34b0bfad2ab68ed6c29d03072695ab2c8faea0c
Monica Dinculescu <noms@google.com>
2018-01-30 14:36:31 -0800

    use polyserve inside the test, actually fix the npm test script

--- test/basic.js
+++ test/basic.js
@@ -10,58 +10,104 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 
 const puppeteer = require('puppeteer');
 const expect = require('chai').expect;
+const {startServer} = require('polyserve');
+const path = require('path');
 var fs = require('fs');
 
 const appUrl = 'http://127.0.0.1:4444';
 
-// Shadow DOM helpers.
-const getShadowRootChildProp = (el, childSelector, prop) => {
-  return el.shadowRoot.querySelector(childSelector)[prop];
-};
-const doShadowRootClick = (el, childSelector) => {
-  return el.shadowRoot.querySelector(childSelector).click();
-};
+describe('basic tests', async function() {
+  let polyserve;
+  before(async function() {
+    polyserve = await startServer({port:4444, root:path.join(__dirname, '..')});
+    console.log('awaited');
+  });
 
-it('the app looks right with the eyeballs', function() {
-  (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+  after(function(done) {
+    polyserve.close(done);
+  });
 
-    const dir = `${process.cwd()}/test/screenshots`;
-    if (!fs.existsSync(dir)){
-        fs.mkdirSync(dir);
-    }
+  it('the app looks right with the eyeballs', function() {
+    (async () => {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
 
-    // See that each route loads correctly.
-    await page.goto(`${appUrl}`);
-    await page.screenshot({path: `${dir}/index.png`});
+      const dir = `${process.cwd()}/test/screenshots`;
+      if (!fs.existsSync(dir)){
+          fs.mkdirSync(dir);
+      }
 
-    for (let i = 1; i <= 3; i++) {
-      await page.goto(`${appUrl}/view${i}`);
-      await page.screenshot({path: `${dir}/view${i}.png`});
-    }
+      // See that each route loads correctly.
+      await page.goto(`${appUrl}`);
+      await page.screenshot({path: `${dir}/index.png`});
 
-    await browser.close();
-  })();
-});
+      for (let i = 1; i <= 3; i++) {
+        await page.goto(`${appUrl}/view${i}`);
+        await page.screenshot({path: `${dir}/view${i}.png`});
+      }
 
-it('the page selector switches pages', function(done) {
-  (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(`${appUrl}`);
-    await page.waitForSelector('my-app', {visible: true});
+      await browser.close();
+    })();
+  });
 
-    await testNavigation(page, 'view2', 'View Two');
-    await testNavigation(page, 'view3', 'View Three');
-    await testNavigation(page, 'view1', 'View One');
+  it('the page selector switches pages', function(done) {
+    (async () => {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+      await page.goto(`${appUrl}`);
+      await page.waitForSelector('my-app', {visible: true});
 
-    done();
-    await browser.close();
-  })();
+      await testNavigation(page, 'view2', 'View Two');
+      await testNavigation(page, 'view3', 'View Three');
+      await testNavigation(page, 'view1', 'View One');
+
+      done();
+      await browser.close();
+    })();
+  });
+
+  it('the page selector switches pages in a different way', function(done) {
+    (async () => {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+      await page.goto(`${appUrl}`);
+      await page.waitForSelector('my-app', {visible: true});
+
+      // Setup
+      await page.evaluate(() => {
+        window.deepQuerySelector = function(query) {
+          const parts = query.split('::shadow');
+          let el = document;
+          for (let i = 0; i < parts.length; i++) {
+            el = el.querySelector(parts[i]);
+            if (i % 2 === 0) {
+              el = el.shadowRoot;
+            }
+          }
+          return el === document ? null : el;
+        }
+        console.log(window.deepQuerySelector);
+      });
+
+      await testNavigationInADifferentWay(page, 'view2', 'View Two');
+      await testNavigationInADifferentWay(page, 'view3', 'View Three');
+      await testNavigationInADifferentWay(page, 'view1', 'View One');
+
+      done();
+      await browser.close();
+    })();
+  });
 });
 
 async function testNavigation(page, href, linkText) {
+  // Shadow DOM helpers.
+  const getShadowRootChildProp = (el, childSelector, prop) => {
+    return el.shadowRoot.querySelector(childSelector)[prop];
+  };
+  const doShadowRootClick = (el, childSelector) => {
+    return el.shadowRoot.querySelector(childSelector).click();
+  };
+
   const selector = `a[href="/${href}"]`;
   const shadowSelector = `a[href="/${href}"]`;
 
@@ -87,35 +133,3 @@ async function testNavigationInADifferentWay(page, href, linkText) {
   let newUrl = await page.evaluate('window.location.href')
   expect(newUrl).equal(`${appUrl}/${href}`);
 }
-
-it('the page selector switches pages in a different way', function(done) {
-  (async () => {
-    const browser = await puppeteer.launch({headless:false});
-    const page = await browser.newPage();
-    await page.goto(`${appUrl}`);
-    await page.waitForSelector('my-app', {visible: true});
-
-    // Setup
-    await page.evaluate(() => {
-      window.deepQuerySelector = function(query) {
-        const parts = query.split('::shadow');
-        let el = document;
-        for (let i = 0; i < parts.length; i++) {
-          el = el.querySelector(parts[i]);
-          if (i % 2 === 0) {
-            el = el.shadowRoot;
-          }
-        }
-        return el === document ? null : el;
-      }
-      console.log(window.deepQuerySelector);
-    });
-
-    await testNavigationInADifferentWay(page, 'view2', 'View Two');
-    await testNavigationInADifferentWay(page, 'view3', 'View Three');
-    await testNavigationInADifferentWay(page, 'view1', 'View One');
-
-    done();
-    await browser.close();
-  })();
-});
