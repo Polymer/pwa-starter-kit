2a9fdcb0a2b47555e615f5a9e175acfaeff2f30e
Monica Dinculescu <noms@google.com>
2018-01-30 15:55:19 -0800

    add pixel diffing

--- test/basic.js
+++ test/basic.js
@@ -13,6 +13,8 @@ const expect = require('chai').expect;
 const {startServer} = require('polyserve');
 const path = require('path');
 const fs = require('fs');
+const PNG = require('pngjs').PNG;
+const pixelmatch = require('pixelmatch');
 
 const appUrl = 'http://127.0.0.1:4444';
 
@@ -37,7 +39,8 @@ describe('basic tests', async function() {
   });
 
   it('the app looks right with the eyeballs', async function() {
-    const dir = `${process.cwd()}/test/screenshots`;
+    const dir = `${process.cwd()}/test/screenshots-current`;
+    const baselineDir = `${process.cwd()}/test/screenshots-baseline`;
     if (!fs.existsSync(dir)){
       fs.mkdirSync(dir);
     }
@@ -49,6 +52,26 @@ describe('basic tests', async function() {
     for (let i = 1; i <= 3; i++) {
       await page.goto(`${appUrl}/view${i}`);
       await page.screenshot({path: `${dir}/view${i}.png`});
+
+      // Compare this screenshot with its baseline.
+      var filesRead = 0;
+      var thisImg = fs.createReadStream(`${dir}/view${i}.png`).pipe(new PNG()).on('parsed', doneReading);
+      var baselineImg = fs.createReadStream(`${baselineDir}/view${i}.png`).pipe(new PNG()).on('parsed', doneReading);
+
+      function doneReading() {
+        // Wait until both files are read;
+        if (++filesRead < 2) return;
+
+        // The files should be the same size.
+        expect(thisImg.width, 'image widths are the same').equal(baselineImg.width);
+        expect(thisImg.height, 'image heights are the same').equal(baselineImg.height);
+
+        // Visual diff
+        var diff = new PNG({width: thisImg.width, height: thisImg.height});
+        var numDiffPixels = pixelmatch(thisImg.data, baselineImg.data, diff.data, thisImg.width, thisImg.height, {threshold: 0.1});
+
+        expect(numDiffPixels, `view${i}.png looks correct`).equal(0);
+      }
     }
   });
 
