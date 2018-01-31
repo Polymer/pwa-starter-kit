ac107f52b9b050930b1457ee932a7a799b30b6c8
Monica Dinculescu <noms@google.com>
2018-01-31 10:45:43 -0800

    try helvetica

--- test/visual.js
+++ test/visual.js
@@ -116,11 +116,11 @@ function compareScreenshots(view) {
       expect(img1.height, 'image heights are the same').equal(img2.height);
 
       // Do the visual diff.
-      const diff = new PNG({width: img1.width, height: img2.height});
-      const numDiffPixels = pixelmatch(img1.data, img2.data, diff.data, img1.width, img1.height, {threshold: 0.1});
+      const diff = new PNG({width: img1.width, height: img1.height});
+      const numDiffPixels = pixelmatch(img1.data, img2.data, diff.data, img1.width, img1.height, {threshold: 0.2});
+      //diff.pack().pipe(fs.createWriteStream(`${currentDir}/${view}-diff.png`));
 
       expect(numDiffPixels, 'number of different pixels').equal(0);
-
       resolve();
     }
   });
