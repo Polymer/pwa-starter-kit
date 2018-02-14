2d39fc71eaf4c5cb3746e9d433253d55aca5a269
Monica Dinculescu <noms@google.com>
2018-02-14 13:03:39 -0800

    remove some of the snackbar styles

--- src/components/snack-bar.js
+++ src/components/snack-bar.js
@@ -30,9 +30,8 @@ class SnackBar extends LitElement {
         left: 0;
         right: 0;
         padding: 12px;
-        background-color: var(--app-secondary-color);
+        background-color: black;
         color: white;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
         text-align: center;
         will-change: transform;
         transform: translate3d(0, 100%, 0);
