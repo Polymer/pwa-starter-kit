87bf6741bf59fd200bc348a7e00ef626442b4dff
Keanu Lee <keanulee@google.com>
2018-06-05 16:52:26 -0700

    Remove await to dispatch before module loads

--- src/actions/app.js
+++ src/actions/app.js
@@ -26,30 +26,26 @@ export const navigate = (path) => (dispatch) => {
   dispatch(updateDrawerState(false));
 };
 
-const loadPage = (page) => async (dispatch) => {
-  // If the page is invalid, set to 404. The is also a good spot to check
-  // other location things like sub-path or query params.
-  if (['view1', 'view2', 'view3'].indexOf(page) === -1) {
-    page = 'view404';
+const loadPage = (page) => (dispatch) => {
+  switch(page) {
+    case 'view1':
+      import('../components/my-view1.js').then((module) => {
+        // Put code in here that you want to run every time when
+        // navigating to view1 after my-view1.js is loaded.
+      });
+      break;
+    case 'view2':
+      import('../components/my-view2.js');
+      break;
+    case 'view3':
+      import('../components/my-view3.js');
+      break;
+    default:
+      page = 'view404';
+      import('../components/my-view404.js');
   }
 
   dispatch(updatePage(page));
-
-  switch(page) {
-    case 'view1':
-      await import('../components/my-view1.js');
-      // Put code here that you want it to run every time when
-      // navigate to view1 page and my-view1.js is loaded
-      break;
-    case 'view2':
-      await import('../components/my-view2.js');
-      break;
-    case 'view3':
-      await import('../components/my-view3.js');
-      break;
-    default:
-      await import('../components/my-view404.js');
-  }
 }
 
 const updatePage = (page) => {
