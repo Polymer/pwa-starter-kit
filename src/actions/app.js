9f6e920e112299387775f41a1a9fbcf69fd55e69
Keanu Lee <keanulee@google.com>
2018-12-10 16:17:01 -0800

    Named chunks

--- src/actions/app.js
+++ src/actions/app.js
@@ -29,20 +29,20 @@ export const navigate = (path) => (dispatch) => {
 const loadPage = (page) => (dispatch) => {
   switch(page) {
     case 'view1':
-      import('../components/my-view1.js').then((module) => {
+      import(/* webpackChunkName: 'src/components/my-view1' */ '../components/my-view1.js').then((module) => {
         // Put code in here that you want to run every time when
         // navigating to view1 after my-view1.js is loaded.
       });
       break;
     case 'view2':
-      import('../components/my-view2.js');
+      import(/* webpackChunkName: 'src/components/my-view2' */ '../components/my-view2.js');
       break;
     case 'view3':
-      import('../components/my-view3.js');
+      import(/* webpackChunkName: 'src/components/my-view3' */ '../components/my-view3.js');
       break;
     default:
       page = 'view404';
-      import('../components/my-view404.js');
+      import(/* webpackChunkName: 'src/components/my-view404' */ '../components/my-view404.js');
   }
 
   dispatch(updatePage(page));
